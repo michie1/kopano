7e6726cef89fdd36ac2b9d8615c7a77c56fc21a4
Michiel <git@fastfox.nl>
2018-03-29 16:30:03 +0200

    Search in subject of items

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import './App.css';
 
-const api = 'http://10.10.50.118:5000';
+const api = 'http://192.168.50.22:5000';
 
 class App extends Component {
   constructor(props) {
@@ -10,7 +10,8 @@ class App extends Component {
     this.state = {
       folders: [],
       items: [],
-      item: null
+      item: null,
+      query: ''
     };
   }
 
@@ -25,8 +26,12 @@ class App extends Component {
       });
 
     const renderItems = this.state.items
+      .filter((item) => {
+        console.log(this.state.query);
+        return this.state.query === '' || item.subject.toLowerCase().includes(this.state.query);
+      })
       .map((item, index) => {
-        return <Item key={index} subject={item.subject} id={item.id} folderId={item.folderId} loadItem={this.loadItem.bind(this)} />;
+        return <Item key={index} subject={item.subject} size={item.size} id={item.id} folderId={item.folderId} loadItem={this.loadItem.bind(this)} />;
       });
 
     const renderItemDetail = (() => {
@@ -40,10 +45,20 @@ class App extends Component {
     return (
       <div>
         <ul>{renderFolders}</ul>
+        <Search onQueryChange={this.search.bind(this)} />
         <ul>{renderItems}</ul>
         {renderItemDetail}
       </div>
     );
+  }
+
+  search(query) {
+    this.setState({
+      folders: this.state.folders,
+      items: this.state.items,
+      item: this.state.item,
+      query: query.toLowerCase()
+    });
   }
 
   loadFolders() {
@@ -59,7 +74,8 @@ class App extends Component {
       this.setState({
         folders,
         items: this.state.items,
-        item: this.state.item
+        item: this.state.item,
+        query: this.state.query
       });
     });
   }
@@ -81,7 +97,8 @@ class App extends Component {
             });
           }),
           folders: this.state.folders,
-          item: this.state.item
+          item: this.state.item,
+          query: this.state.query
         });
       });
   }
@@ -99,11 +116,9 @@ class App extends Component {
         this.setState({
           items: this.state.items,
           folders: this.state.folders,
-          item
+          item,
+          query: this.state.query
         });
-      })
-      .catch((e) => {
-        console.log('e', e);
       });
   }
 }
@@ -133,22 +148,33 @@ class Item extends Component {
 
   render() {
     return <li>
-              <a href="#" onClick={this.onItemClick(this.props.folderId, this.props.id)}>{this.props.subject}</a>
+              <a href="#" onClick={this.onItemClick(this.props.folderId, this.props.id)}>{this.props.subject} ({this.props.size})</a>
            </li>;
   }
 }
 
 class ItemDetail extends Component {
   render() {
-    console.log(this.props.item);
+    const receiver = this.props.item.recipients[0];
     return <div>
               <h2>{this.props.item.subject}</h2>
               <ul>
-                <li>{this.props.item.sender.name} ({this.props.item.sender.email})</li>
+                <li>Sender: {this.props.item.sender.name} ({this.props.item.sender.email})</li>
+                <li>Receiver: {receiver.name} ({receiver.email})</li>
                 <li>{this.props.item.received}</li>
               </ul>
               <div>{ReactHtmlParser(this.props.item.html)}</div>
             </div>;
+  }
+}
+
+class Search extends Component {
+  render() {
+    return <div>
+              <span>Search</span>
+              <input type="text" onChange={(e) => { return this.props.onQueryChange(e.target.value)}} />
+            </div>;
+
   }
 }
 
