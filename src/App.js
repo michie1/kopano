e44e7c4d37734289dccaa353b5ac5a052fa81718
Michiel <git@fastfox.nl>
2018-03-29 16:05:17 +0200

    Load and render folders, folder and item

--- src/App.js
+++ src/App.js
@@ -1,20 +1,154 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import './App.css';
 
+const api = 'http://10.10.50.118:5000';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      folders: [],
+      items: [],
+      item: null
+    };
+  }
+
+  componentDidMount() {
+    this.loadFolders();
+  }
+
   render() {
+    const renderFolders = this.state.folders
+      .map((folder, index) => {
+        return <Folder key={index} name={folder.name} id={folder.id} loadFolder={this.loadFolder.bind(this)} />;
+      });
+
+    const renderItems = this.state.items
+      .map((item, index) => {
+        return <Item key={index} subject={item.subject} id={item.id} folderId={item.folderId} loadItem={this.loadItem.bind(this)} />;
+      });
+
+    const renderItemDetail = (() => {
+      if (this.state.item === null) {
+        return <span>No item selected</span>;
+      } else {
+        return <ItemDetail key={this.state.item.id} item={this.state.item} />;
+      }
+    })();
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ul>{renderFolders}</ul>
+        <ul>{renderItems}</ul>
+        {renderItemDetail}
       </div>
     );
+  }
+
+  loadFolders() {
+    const url = api + '/folders/'; // End slash is required to prevent redirect and cors problems;
+    return fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+    .then((response) => {
+      return response.folders;
+    })
+    .then((folders) => {
+      this.setState({
+        folders,
+        items: this.state.items,
+        item: this.state.item
+      });
+    });
+  }
+
+  loadFolder(id) {
+    const url = api + '/folder/' + id + '/items';
+    return fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        return response.items;
+      })
+      .then((items) => {
+        this.setState({
+          items: items.map((item) => {
+            return Object.assign(item, {
+              folderId: id
+            });
+          }),
+          folders: this.state.folders,
+          item: this.state.item
+        });
+      });
+  }
+
+  loadItem(folderId, id) {
+    const url = api + '/folder/' + folderId + '/item/' + id;
+    return fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        return response.item;
+      })
+      .then((item) => {
+        this.setState({
+          items: this.state.items,
+          folders: this.state.folders,
+          item
+        });
+      })
+      .catch((e) => {
+        console.log('e', e);
+      });
+  }
+}
+
+class Folder extends Component {
+  onFolderClick(id) {
+    return (e) => {
+      e.preventDefault();
+      return this.props.loadFolder(id);
+    }
+  }
+
+  render() {
+    return <li>
+              <a href="#" onClick={this.onFolderClick(this.props.id)}>{this.props.name}</a>
+           </li>;
+  }
+}
+
+class Item extends Component {
+  onItemClick(folderId, id) {
+    return (e) => {
+      e.preventDefault();
+      return this.props.loadItem(folderId, id);
+    }
+  }
+
+  render() {
+    return <li>
+              <a href="#" onClick={this.onItemClick(this.props.folderId, this.props.id)}>{this.props.subject}</a>
+           </li>;
+  }
+}
+
+class ItemDetail extends Component {
+  render() {
+    console.log(this.props.item);
+    return <div>
+              <h2>{this.props.item.subject}</h2>
+              <ul>
+                <li>{this.props.item.sender.name} ({this.props.item.sender.email})</li>
+                <li>{this.props.item.received}</li>
+              </ul>
+              <div>{ReactHtmlParser(this.props.item.html)}</div>
+            </div>;
   }
 }
 
